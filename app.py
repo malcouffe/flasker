647c83a4b316fa84f60fe4ab3e2e9036dbeb4790
Matthieu Alcouffe <mat91700@gmail.com>
2022-12-15 16:20:05 +0100

    Fichiers static et better code

--- app.py
+++ app.py
@@ -1,10 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # Création d'un instance flask 
 app = Flask(__name__)
+# Ajout de la base de données
+# Ancienne bdd SQLite
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
+# Nouvelle bdd mySQL
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:SQL123@localhost/users'
+
+# Clé secrète 
+app.config['SECRET_KEY'] = "mot de passe à ne pas partager"
+
+# Initialiter la base de données 
+db = SQLAlchemy(app)
+
+
+
+# Création d'un modèle 
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(200), nullable = False)
+    email = db.Column(db.String(120), nullable = False, unique = True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+# Création d'une classe Form
+class UserForm(FlaskForm):
+    name = StringField("Nom", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+# Création d'une classe Form
+class NamerForm(FlaskForm):
+    name = StringField("Quel est votre nom ?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 # Création d'une route 
-
 @app.route('/')
 def index():
     first_name = "Mat"
@@ -28,3 +69,33 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validation du formulaire 
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Enregistrement réussi")
+    return render_template('name.html', 
+        name = name, 
+        form = form)
+
+# Page ajout d'un utilisateur 
+@app.route('/user/add', methods = ['GET', 'POST'])
+def add_user():
+    name = None
+    form = UserForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email = form.email.data).first() # test si l'adresse mail existe déjà dans la bdd, ne devrait rien renvoyer si pas d'adresse dans la bdd
+        if user is None:
+            user = Users(name = form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ''
+        form.email.data = ''
+    our_users = Users.query.order_by(Users.date_added)
+    return render_template("add_user.html", form = form, name=name, our_users=our_users)
